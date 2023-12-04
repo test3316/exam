9fe76bc8dd200ab5821c2586517e0189c1a66aa2
Giorgi Kochiashvili <qochoo@gmail.com>
2023-12-04 23:14:47 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Fetch from "./Components/Fetch/Fetch";
+import InputButtons from "./Components/Input/InputButtons";
+import "./App.css";
+function App() {
+  return (
+    <>
+      <div>
+        <InputButtons />
+        <Fetch />
+      </div>
+    </>
+  );
+}
+
+export default App;
